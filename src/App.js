4fb02f49cda8e9b7f1999fb3dc49b40638fdb416
Nathan Schrader <natecschrader@gmail.com>
2023-08-24 16:13:01 -0700

    add individual Routes in react-router

--- src/App.js
+++ src/App.js
@@ -41,7 +41,29 @@ const App = () => {
           </div>
           <div>
             <Routes>
+              {/* Dashboard */}
               <Route path="/" element="Ecommerce" />
+              <Route path="/Ecommerce" element="Ecommerce" />
+              {/* Pages */}
+              <Route path="/orders" element="orders" />
+              <Route path="/employees" element="Employees" />
+              <Route path="/customers" element="Customers" />
+
+              {/* Apps */}
+              <Route path="//kanban" element="Kanban" />
+              <Route path="/editor" element="Editor" />
+              <Route path="/calendar" element="Calendar" />
+              <Route path="/color-picker" element="ColorPicker" />
+
+              {/* Charts */}
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/pie" element="Pie" />
+              <Route path="/financial" element="Financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
             </Routes>
           </div>
         </div>
