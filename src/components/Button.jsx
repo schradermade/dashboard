30c71b1c0f4f359c160386e24936a34af282987b
Nathan Schrader <natecschrader@gmail.com>
2023-08-25 13:53:25 -0700

    built out button comp

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ bgColor, color, size, text, borderRadius, width }) => {
   return (
-    <div>Button</div>
-  )
-}
+    <div
+      type="button"
+      style={{ backgroundColor: bgColor, bgColor, color, borderRadius }}
+      className={`text-${size} w-${width} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </div>
+  );
+};
 
-export default Button
+export default Button;
